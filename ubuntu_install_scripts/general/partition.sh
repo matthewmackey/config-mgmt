a3abd2a79ec498c302587c9635579417a5ceb184
Matthew Machaj <73896224+matthewmachaj@users.noreply.github.com>
2022-01-20 21:52:38 -0500

    ubuntu_scripts: finished update to general partition script

--- ubuntu_install_scripts/general/partition.sh
+++ ubuntu_install_scripts/general/partition.sh
@@ -28,23 +28,23 @@ EOF
 
 verify_vars_provided() {
   REQUIRED_VARS=(
+    BOOT_PART_NUM
+    ROOTFS_PART_NUM
+    DEV
     BOOT_DEV
-    BOOT_CRYPT_SIZE
     ROOTFS_DEV
-    ROOTFS_CRYPT_SIZE
+    BOOT_PART_NAME
+    BOOT_PART_SIZE
+    ROOTFS_PART_NAME
     SWAP_LVM_SIZE
     ROOT_LVM_SIZE
     HOME_LVM_SIZE
+    DOCKER_LVM_SIZE
   )
 
   OPTIONAL_VARS=(
     PASSPHRASE_FILE
   )
-
-  OPTIONAL_ARRAY_VARS=(
-    ROOTFS_CRYPT_EXTRA_LVM_PARTITIONS
-  )
-
 
   for v in ${REQUIRED_VARS[@]}; do
     if [ "${!v}" = "" ]; then
@@ -59,17 +59,6 @@ verify_vars_provided() {
     if [ "${!v}" != "" ]; then
       echo "OPTIONAL variable $v found"
       echo "  ${!v}"
-    fi
-  done
-
-  for v in ${OPTIONAL_ARRAY_VARS[@]}; do
-    _arr_ref="$v[@]"
-    _arr=(${!_arr_ref})
-    _arr_len="${#_arr[@]}"
-
-    if [ $_arr_len -gt 0 ]; then
-      echo "OPTIONAL array variable $v found"
-      echo "  ${_arr[@]}"
     fi
   done
 }
@@ -89,30 +78,6 @@ error() {
   echo
   usage
   exit 1
-}
-
-
-#-----------------------------------------------------------------------
-# Step:
-#-----------------------------------------------------------------------
-create_partitions() {
-  header "[Create Partitions]"
-
-  # /boot - encrypted
-  sgdisk --new 5:0:+${BOOT_PART_SIZE} $DEV
-
-  # EMPTY partition for future use
-  sgdisk --new 7:-${EMPTY_PART_SIZE}:0 $DEV
-
-  # / - encrypted (declared after part #7 so can fill rest of space)
-  sgdisk --new 6:0:0 $DEV
-
-  sgdisk --typecode 5:8300 $DEV
-  sgdisk --typecode 6:8300 $DEV
-
-  sgdisk --change-name 5:"Ubuntu 20.04 /boot" $DEV
-  sgdisk --change-name 6:"Ubuntu 20.04 /" $DEV
-  sgdisk --change-name 7:"EMPTY" $DEV
 }
 
 
@@ -147,6 +112,29 @@ read_luks_passphrase() {
 #-----------------------------------------------------------------------
 # Step:
 #-----------------------------------------------------------------------
+create_partitions() {
+  header "[Create Partitions]"
+
+  # /boot (encrypted - LUKS v1)
+  #
+  # <part_num>:0:+<part_size> -> create partition of size <part_size> after
+  #                              end of last partition
+  sgdisk --new ${BOOT_PART_NUM}:0:+${BOOT_PART_SIZE} $DEV
+  sgdisk --change-name ${BOOT_PART_NUM}:"${BOOT_PART_NAME}" $DEV
+  sgdisk --typecode ${BOOT_PART_NUM}:8300 $DEV
+
+  # / (encrypted - LUKS v2)
+  #
+  # <part_num>:0:0 -> fill remaining space
+  sgdisk --new ${ROOTFS_PART_NUM}:0:0 $DEV
+  sgdisk --change-name ${ROOTFS_PART_NUM}:"${ROOTFS_PART_NAME}" $DEV
+  sgdisk --typecode ${ROOTFS_PART_NUM}:8300 $DEV
+}
+
+
+#-----------------------------------------------------------------------
+# Step:
+#-----------------------------------------------------------------------
 create_luks() {
   header "[LUKS Format]"
   echo -n "$PASSPHRASE" | cryptsetup luksFormat --type=luks1 $BOOT_DEV
@@ -159,8 +147,8 @@ create_luks() {
 #-----------------------------------------------------------------------
 open_luks() {
   header "[LUKS Open]"
-  echo -n "$PASSPHRASE" | cryptsetup open $BOOT_DEV $BOOT_DM
-  echo -n "$PASSPHRASE" | cryptsetup open $ROOTFS_DEV $ROOTFS_DM
+  echo -n "$PASSPHRASE" | cryptsetup open $BOOT_DEV $BOOT_DEV_MAPPER
+  echo -n "$PASSPHRASE" | cryptsetup open $ROOTFS_DEV $ROOTFS_DEV_MAPPER
 }
 
 
@@ -168,40 +156,26 @@ open_luks() {
 # Step:
 #-----------------------------------------------------------------------
 format_boot_partition() {
-  echo -e "\n------ [Format /boot] --------------------------"
-  mkfs.ext4 -L boot /dev/mapper/$BOOT_DM
+  header "[Format /boot]"
+  mkfs.ext4 -L boot /dev/mapper/$BOOT_DEV_MAPPER
 }
 
 
 #-----------------------------------------------------------------------
 # Step:
 #-----------------------------------------------------------------------
-setup_root_lvm() {
-  echo -e "\n------ [Setup root LVM] --------------------------"
-  LVM_DM=/dev/mapper/$ROOTFS_DM
-  VG_NAME=ubuntu-vg
+# Formatting done by OS installer
+setup_rootfs_lvm() {
+  header "[Setup rootfs LVM]"
 
-  pvcreate $LVM_DM
+  pvcreate $PV
 
-  vgcreate $VG_NAME $LVM_DM
+  vgcreate $VG_NAME $PV
 
-  lvcreate -L $SWAP_SIZE -n swap $VG_NAME
-  lvcreate -L $ROOT_PART_SIZE -n root $VG_NAME
-  lvcreate -L $HOME_PART_SIZE -n home $VG_NAME
-}
-
-
-#-----------------------------------------------------------------------
-# Step:
-#-----------------------------------------------------------------------
-display_grub_message() {
-  echo -e "\n------ [GRUB message] --------------------------"
-  cat <<-END
-    After you start the installer, you need to come back to the
-    echo terminal and run:
-
-    echo "GRUB_ENABLE_CRYPTODISK=y" >> /target/etc/default/grub
-	END
+  lvcreate -L $SWAP_LVM_SIZE -n swap $VG_NAME
+  lvcreate -L $ROOT_LVM_SIZE -n root $VG_NAME
+  lvcreate -L $HOME_LVM_SIZE -n home $VG_NAME
+  lvcreate -L $DOCKER_LVM_SIZE -n docker $VG_NAME
 }
 
 
@@ -214,13 +188,13 @@ undo() {
 
   # Delete LVM
   echo -e "\n------ [Delete LVM] --------------------------"
-  lvremove -y /dev/mapper/ubuntu--vg-*
-  vgremove -y ubuntu-vg
-  pvremove -y /dev/mapper/$ROOTFS_DM
+  lvremove -y /dev/mapper/$VG_NAME-*
+  vgremove -y $VG_NAME
+  pvremove -y $PV
 
   # Close LUKS devices
   echo -e "\n------ [Close LUKS devices] --------------------------"
-  for i in $BOOT_DM $ROOTFS_DM; do
+  for i in $BOOT_DEV_MAPPER $ROOTFS_DEV_MAPPER; do
     cryptsetup close /dev/mapper/$i
   done
 
@@ -232,7 +206,7 @@ undo() {
 
   # Delete partitions
   echo -e "\n------ [Delete partitions] --------------------------"
-  for i in 5 6 7; do
+  for i in $BOOT_PART_NUM $ROOTFS_PART_NUM; do
     sgdisk --delete $i $DEV
   done
 }
@@ -243,11 +217,11 @@ undo() {
 #-----------------------------------------------------------------------
 main() {
   read_luks_passphrase
-  # create_partitions
-  # create_luks
-  # open_luks
-  # format_boot_partition
-  # setup_root_lvm
+  create_partitions
+  create_luks
+  open_luks
+  format_boot_partition
+  setup_rootfs_lvm
   display_grub_message
 }
 
@@ -256,30 +230,41 @@ main() {
 # CLI
 #-----------------------------------------------------------------------
 
-BOOT_DM=bootcrypt_luks1
-ROOTFS_DM=rootcrypt
+BOOT_DEV_MAPPER=bootcrypt_luks1
+ROOTFS_DEV_MAPPER=rootcrypt
+PV=/dev/mapper/$ROOTFS_DEV_MAPPER
+VG_NAME=ubuntuvg
+
+# Placeholder global var
+PASSPHRASE=
 
 #-----------------------------
 # REQUIRED Variables
 #-----------------------------
 # Using ROOTFS_* vs. ROOT_*, which breaks the convention of how I relate those
-# vars to the DM names below; doing this b/c it would be to easy to confuse
+# vars to the DEV_MAPPER names below; doing this b/c it would be to easy to confuse
 # variables named BOOT_* and ROOT_* when reading/editing this script, and I
 # want to try to avoid any confusion that could cause developer errors
-BOOT_DEV=
-BOOT_CRYPT_SIZE=
+BOOT_PART_NUM=
+ROOTFS_PART_NUM=
 
+DEV=
+BOOT_DEV=
 ROOTFS_DEV=
-ROOTFS_CRYPT_SIZE=
+
+BOOT_PART_NAME=
+BOOT_PART_SIZE=
+
+ROOTFS_PART_NAME=
 
 SWAP_LVM_SIZE=
 ROOT_LVM_SIZE=
 HOME_LVM_SIZE=
+DOCKER_LVM_SIZE=
 
 #-----------------------------
 # OPTIONAL Variables
 #-----------------------------
-ROOTFS_CRYPT_EXTRA_LVM_PARTITIONS=()
 PASSPHRASE_FILE=
 
 

cdaef4791cf76e4174e01690c6e4e8466b1c0cc0
Matthew Mackey <21043873+matthewmackey@users.noreply.github.com>
2024-09-20 13:10:18 -0400

    printer: add role to install HP printer CLI/GUI

--- ansible_root/TODO/printer.sh
+++
@@ -1,5 +0,0 @@
-# Install HP Linux CLI utlities and GUI (HP Toolbox)
-# I think hplip (CLI) package comes installed in Ubuntu by default
-#
-# See: https://developers.hp.com/hp-linux-imaging-and-printing
-sudo apt install -y hplip hplip-gui
